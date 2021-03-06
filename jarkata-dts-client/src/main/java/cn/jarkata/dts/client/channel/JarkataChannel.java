3e73f1bd1ce35965795fba2ae0f065e809ea1338
vkata <fx.windy@foxmail.com>
2021-03-06 15:53:31 +0800

    add common utils

--- jarkata-dts-client/src/main/java/cn/jarkata/dts/client/channel/JarkataChannel.java
+++ jarkata-dts-client/src/main/java/cn/jarkata/dts/client/channel/JarkataChannel.java
@@ -6,16 +6,16 @@ import cn.jarkata.protobuf.DataMessage;
 import cn.jarkata.protobuf.utils.ProtobufUtils;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -38,6 +38,12 @@ public class JarkataChannel {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.group(new NioEventLoopGroup());
         bootstrap.channel(NioSocketChannel.class);
+        bootstrap.option(ChannelOption.ALLOCATOR, ByteBufAllocator.DEFAULT)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.SO_REUSEADDR, true)
+                .option(ChannelOption.SO_RCVBUF, 8 * 1024)
+                .option(ChannelOption.SO_SNDBUF, 8 * 1024);
         bootstrap.handler(new ClientHandlerInitializer(handler));
         ChannelFuture channelFuture = bootstrap.connect(host, port);
         ChannelFuture future = channelFuture.sync();
