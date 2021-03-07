a1ee767a6ea6437174d73592589c3f874d0c13d8
vkata <fx.windy@foxmail.com>
2021-03-08 00:30:21 +0800

    add build

--- jarkata-dts-client/src/test/java/cn/jarkata/dts/client/DTSClientTest.java
+++ jarkata-dts-client/src/test/java/cn/jarkata/dts/client/DTSClientTest.java
@@ -1,9 +1,13 @@
 package cn.jarkata.dts.client;
 
+import cn.jarkata.commons.utils.NetUtils;
 import cn.jarkata.dts.common.Env;
 import cn.jarkata.dts.common.utils.FileUtils;
+import io.netty.util.NetUtil;
 import org.junit.Test;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
 import java.util.List;
 
 import static cn.jarkata.dts.common.constant.JarkataConstant.CLIENT_BASE_PATH;
@@ -23,5 +27,15 @@ public class DTSClientTest {
             if (NettyClient.allowTransfer(basePath, fullPath)) {
             }
         }
+    }
+
+    @Test
+    public void test() throws Exception {
+        InetAddress localHost = InetAddress.getLocalHost();
+        System.out.println(localHost);
+        String inet4Address = NetUtils.getInet4Address();
+        Inet6Address byName = NetUtil.getByName("182.168.0.1");
+        System.out.println(byName);
+        System.out.println(inet4Address);
     }
 }
