9d58112913d863d08b4b52ed73fb2bcc3d19571c
vkata <fx.windy@foxmail.com>
2021-03-07 21:57:14 +0800

    add build

--- jarkata-dts-server/src/main/java/cn/jarkata/dts/DTSStarter.java
+++ jarkata-dts-server/src/main/java/cn/jarkata/dts/DTSStarter.java
@@ -3,16 +3,19 @@ package cn.jarkata.dts;
 
 import cn.jarkata.dts.common.Env;
 import cn.jarkata.dts.server.NettyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static cn.jarkata.dts.common.constant.JarkataConstant.SERVER_PORT;
 import static cn.jarkata.dts.common.constant.JarkataConstant.SERVER_PORT_DEFAULT_VAL;
 
 public class DTSStarter {
+    private static final Logger logger = LoggerFactory.getLogger(DTSStarter.class);
 
     public static void main(String[] args) {
         String serverPort = Env.getProperty(SERVER_PORT, SERVER_PORT_DEFAULT_VAL);
         int port = Integer.parseInt(serverPort);
         new NettyServer(port).start();
-        System.out.println("starting-----end");
+        logger.info("server start success");
     }
 }
